676a6369b092dc63d93961a808670703115f9efe
Samyog <74084097+safalsamyog@users.noreply.github.com>
2022-08-04 17:04:58 +0545

    Add files via upload

---
+++ userform_profile/app1/forms.py
@@ -0,0 +1,18 @@
+#from dataclasses import field
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from django import forms
+class UForm(UserCreationForm):
+	password2=forms.CharField(label='confirm password',widget=forms.PasswordInput)
+	class Meta:
+		model=User
+		fields=['username','first_name','last_name','email']
+		labels={'email':'email'}
+class User_Change(UserChangeForm):
+	password=None
+	class Meta:
+		model=User
+		fields=['first_name','last_name','email','date_joined','last_login']
+		labels={'email':'email'}
+    	
+
---
+++ userform_profile/app1/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,PasswordChangeForm,UserChangeForm
+from django.contrib.auth import authenticate,login,logout,update_session_auth_hash
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from .forms import UForm,User_Change
+# Create your views here.
+
+def SignUp(request):
+    if request.method=='POST':
+        form=UForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Signup successfully')
+    else:
+        form=UForm()
+    return render(request,'app1/signup.html',{'form':form})
+
+def log_in(request):
+    if not request.user.is_authenticated:
+        if request.method=='POST':
+            form2=AuthenticationForm(request=request,data=request.POST)#in this form we need to give two parameters
+            if form2.is_valid():
+                username=form2.cleaned_data['username']
+                password=form2.cleaned_data['password']
+            
+                user=authenticate(username=username,password=password)
+            
+                if user is not None:
+                    login(request,user)
+                    return HttpResponseRedirect('/profile/')
+        else:
+            form2=AuthenticationForm()
+    else:
+        return HttpResponseRedirect('/login/')
+    return render(request,'app1/login.html',{'form2':form2})
+
+def profile(request):
+    if request.user.is_authenticated:
+        if request.method=='POST':
+            form=User_Change(request.POST,instance=request.user)
+            if form.is_valid():
+                form.save()
+                messages.success(request,'Saved successfully')
+       
+        else:
+            form=User_Change(instance=request.user)
+        return render(request,'app1/profile.html',{'name':request.user,'form':form})
+       
+    else:
+        return HttpResponseRedirect('/login/')
+
+
+def log_out(request):
+    logout(request)
+    return HttpResponseRedirect('/login/')
+
+def change_password(request):
+    if request.user.is_authenticated:
+        if request.method=='POST':
+            form=PasswordChangeForm(user=request.user,data=request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request,'password change successfully')
+                update_session_auth_hash(request,form.user)
+                return HttpResponseRedirect('/profile/')#this is will not redirect becuase session will be wiped out so need to keep updated
+        else:
+            form=PasswordChangeForm(user=request.user)
+    
+        return render(request,'app1/changepassword.html',{'form':form})
+    else:
+        return HttpResponseRedirect('/login/')
+
+
